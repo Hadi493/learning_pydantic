1584b4fbffce3fc5f49bdf3a0d84759db03cba61
Hadi493 <hadialam493@gmail.com>
2025-05-03 00:36:16 +0600

    fields validation

---
+++ fields_validation_02/assignments/employee_model_starter.py
@@ -0,0 +1,8 @@
+# from pydantic import BaseModel
+
+# TODO: Create employee model
+# Fields:
+# - id: int
+# - name: str (min 3 chars)
+# - department: Optional str (default 'general')
+# - salary: float (must be >= 10000)
---
+++ fields_validation_02/solutions/employee_model_solution.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+# TODO: Create employee model
+# Fields:
+# - id: int
+# - name: str (min 3 chars)
+# - department: Optional str (default 'general')
+# - salary: float (must be >= 10000)
+
+
+class Employee(BaseModel):
+    id: int
+    name: str = Field(..., min_length=3)
+    department: Optional[str] = 'General'
+    salary: float = Field(..., ge='10000')
