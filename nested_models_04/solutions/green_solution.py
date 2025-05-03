62e21b4388af35854b540d58d83d5dd09965b9dc
Hadi493 <hadialam493@gmail.com>
2025-05-03 20:29:31 +0600

    assignment solution

--- nested_models_04/solutions/green_solution.py
+++ nested_models_04/solutions/green_solution.py
@@ -4,3 +4,20 @@ from typing import List
 # TODO: Create course model
 # Each course has modules
 # Each modules has lessons
+
+
+class Lesson(BaseModel):
+    lesson_id: int
+    topic: str
+
+
+class Module(BaseModel):
+    module_id: int
+    name: str
+    lessons: List[Lesson]
+
+
+class Course(BaseModel):
+    course_id: int
+    title: str
+    modules: List[Module]
